9e6f62630d222cdfe6ea0939ad8c31c5201e3cd7
omar askar <oaskar120@gmail.com>
2025-01-19 01:52:59 +0200

    Question part

---
+++ examanswer/MCQ.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examanswer
+{
+    internal class MCQ : Question
+    {
+        public MCQ(string header, string body, int mark) : base(header, body, mark)
+        {
+        }
+
+
+    }
+}
